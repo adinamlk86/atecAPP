9d9877541ca133776946a6719ea070ba5bac8ed7
adinamlk86 <adina.dumitrescu1@gmail.com>
2019-06-19 19:41:56 +0300

    first upload

---
+++ src/main/java/com/atec/AtecApplication.java
@@ -0,0 +1,13 @@
+package com.atec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtecApplication.class, args);
+	}
+
+}
