edb23e34155cf221dd03d43a28fdc3fd48f2b7ee
Adina <adina.dumitrescu1@gmail.com>
2019-07-02 05:35:35 +0300

    added creating equipment with selected clientId functionality.

--- atecAPP-server/src/main/java/com/atec/service/ClientService.java
+++ atecAPP-server/src/main/java/com/atec/service/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +19,9 @@ public class ClientService {
 	
 	@Autowired
 	private ClientRepository repository;
-	
-	
+
+
+	//@Qualifier("clientMapper")
 	@Autowired
 	private ClientMapper mapper;
 	
--- atecAPP-server/src/main/java/com/atec/service/EquipmentService.java
+++ atecAPP-server/src/main/java/com/atec/service/EquipmentService.java
@@ -2,12 +2,16 @@ package com.atec.service;
 
 import com.atec.dto.EquipmentDTO;
 import com.atec.mapper.EquipmentMapper;
+import com.atec.model.Client;
 import com.atec.model.Equipment;
+import com.atec.repository.ClientRepository;
 import com.atec.repository.EquipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,38 +19,59 @@ import java.util.Optional;
 @Transactional
 public class EquipmentService {
 
+    private ClientRepository clientRepository;
+
     @Autowired
-    private EquipmentRepository repository;
+    private EquipmentRepository equipmentRepository;
 
     @Autowired
     private EquipmentMapper mapper;
 
     @Transactional(readOnly = true)
     public List<EquipmentDTO> getAll(){
-        List<Equipment> dbEquipment = repository.findAll();
+        List<Equipment> dbEquipment = equipmentRepository.findAll();
 
         return mapper.toEquipmentDTOs(dbEquipment);
     }
 
 
     public Equipment getEquipmentById (Long id){
-        Optional<Equipment> optionalEquipment = repository.findById(id);
+        Optional<Equipment> optionalEquipment = equipmentRepository.findById(id);
         return optionalEquipment.orElseThrow(()-> new EquipmentNotFoundException("Couldn't find any equipment with that id."));
     }
 
     public Equipment add(EquipmentDTO equipmentDTO){
 
         Equipment newEquipment =this.mapper.toEquipment(equipmentDTO);
-        this.repository.save(newEquipment);
+        this.equipmentRepository.save(newEquipment);
 
         return newEquipment;
     }
 
 
     public void delete(long id){
-        repository.deleteById(id);
+        equipmentRepository.deleteById(id);
 
     }
 
+    public List<EquipmentDTO> getEquipmentByClient(Long clientId) throws NullPointerException{
+        List<Equipment> equipmentList = new ArrayList<>();
+
+        Client client = null;
+        try {
+            client = this.clientRepository.findById(clientId).orElseThrow(() -> {
+                 return new NullPointerException();});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(client!=null) {
+            equipmentList = this.equipmentRepository.findAllByClientId(client.getId());
+        }
+            List<EquipmentDTO> equipmentDTOList = this.mapper.toEquipmentDTOs(equipmentList);
+
+        return equipmentDTOList;
+
+    }
 
 }
